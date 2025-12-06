2b5ad2f9a2eddee8f9ffcc2864d01f0fabc28750
z0nT1x <plmq7147@gmail.com>
2025-12-06 12:13:43 +0200

    Updating structure

--- pyMatan/limits_derivatives/__init__.py
+++ pyMatan/limits_derivatives/__init__.py
@@ -3,5 +3,6 @@ from .limits import numerical_limit
 from .derivatives import central_difference
 from .derivatives import visualize_derivative
 from .derivatives import forward_difference
+from .derivatives import symbolic_derivative
 from .limits import visualize_limit
 from .limits import symbolic_limit
